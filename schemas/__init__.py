155888ba1136797adc0f93ad1a2c3b8933154a8d
Ester Gomes <ester10gomes@gmail.com>
2025-12-04 14:58:54 -0300

    schema import

---
+++ schemas/__init__.py
@@ -0,0 +1,5 @@
+from schemas.student import SchemaStudentView,  SchemaStudentCreate # noqa
+from schemas.guardian import SchemaGuardianCreate # noqa
+from schemas.contact import SchemaContactCreate # noqa
+from schemas.address import SchemaAddressView,  SchemaAddressCreate # noqa
+

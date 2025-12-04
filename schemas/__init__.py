d1263a8f8fd5ee181dcf388d3ca5b3e032249eb5
Ester Gomes <ester10gomes@gmail.com>
2025-12-04 20:18:29 -0300

    schema query params schema response get student

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,4 +1,4 @@
-from schemas.student import SchemaStudentView,  SchemaStudentCreate # noqa
+from schemas.student import SchemaStudentView,  SchemaStudentCreate, SchemaStudentQueryResponse, SchemaStudentQueryParam # noqa
 from schemas.guardian import SchemaGuardianCreate # noqa
 from schemas.contact import SchemaContactCreate # noqa
 from schemas.address import SchemaAddressView,  SchemaAddressCreate # noqa

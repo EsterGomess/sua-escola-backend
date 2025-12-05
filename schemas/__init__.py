38d6bbe75d71f1aaabd594497419299e4ce62ffa
Ester Gomes <ester10gomes@gmail.com>
2025-12-04 21:32:33 -0300

    import SchemaStudentDeleteParam

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,4 +1,9 @@
-from schemas.student import SchemaStudentView,  SchemaStudentCreate, SchemaStudentQueryResponse, SchemaStudentQueryParam, SchemaStudentUpdate# noqa
+from schemas.student import (SchemaStudentView,  # noqa
+                             SchemaStudentCreate, # noqa
+                             SchemaStudentQueryResponse, # noqa
+                             SchemaStudentQueryParam, # noqa
+                             SchemaStudentUpdate, # noqa
+                             SchemaStudentDeleteParam) # noqa
 from schemas.guardian import SchemaGuardianCreate # noqa
 from schemas.contact import SchemaContactCreate, SchemaContactUpdate # noqa
 from schemas.address import SchemaAddressView,  SchemaAddressCreate , SchemaAddressUpdate# noqa
